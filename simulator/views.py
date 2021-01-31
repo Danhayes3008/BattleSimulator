302d49fbe704a41f978fc28b9075ac71b547773c
danhayes <danhayes3008@outlook.com>
2021-01-31 14:56:32 +0000

    linked the warship model to the index page

--- simulator/views.py
+++ simulator/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from simulator.forms import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+
+    war = Warship.objects.all()
+    return render(request, 'index.html', {'warship': war})
