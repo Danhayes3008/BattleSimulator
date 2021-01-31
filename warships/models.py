b4a83023b42d56e962f75ddd457f42d5875ddd67
danhayes <danhayes3008@outlook.com>
2021-01-31 14:07:49 +0000

    created the model that will allow me to make a database of warships

--- warships/models.py
+++ warships/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Warship(models.Model): 
+   name = models.CharField(max_length=250, blank=True)
+   leadership = models.DecimalField(max_digits=2, decimal_places=0, default='10')
+   power = models.DecimalField(max_digits=4, decimal_places=0, default='100')
+   attackBonus = models.DecimalField(max_digits=4, decimal_places=0, default='100')
+   hp = models.DecimalField(max_digits=4, decimal_places=0, default='100')
+   armour = models.DecimalField(max_digits=4, decimal_places=0, default='100')
+   shield = models.DecimalField(max_digits=4, decimal_places=0, default='100')
+   load = models.DecimalField(max_digits=4, decimal_places=0, default='100')
+   speed = models.DecimalField(max_digits=4, decimal_places=0, default='100')
+   damage = models.DecimalField(max_digits=4, decimal_places=0, default='100')
+
+   class Meta:
+        verbose_name_plural = "Warship"
+
+   def __str__(self):
+        return self.name
